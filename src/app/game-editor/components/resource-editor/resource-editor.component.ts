aae5634f370c9a77b56c92ef845cf5d1b1b75568
Zdravko <zdravkokirilov@yahoo.com>
2017-10-16 20:06:57 +0300

    latest changes

--- src/app/game-editor/components/resource-editor/resource-editor.component.ts
+++ src/app/game-editor/components/resource-editor/resource-editor.component.ts
@@ -7,27 +7,27 @@ import { SaveResourceAction } from '../../state/actions/actions';
 import { Resource } from '../../../game-mechanics/models/Resource';
 
 @Component({
-	selector: 'rg-resource-editor',
-	templateUrl: './resource-editor.component.html',
-	styleUrls: ['./resource-editor.component.scss']
+    selector: 'rg-resource-editor',
+    templateUrl: './resource-editor.component.html',
+    styleUrls: ['./resource-editor.component.scss']
 })
 export class ResourceEditorComponent {
-	public rForm: FormGroup;
-	constructor(private fb: FormBuilder, private store: Store<AppState>) {
-		this.rForm = fb.group({
-			'name': [null, vd.compose([vd.required, vd.minLength(3)])],
-			'description': [null],
-			'plural': [null],
-			'image': [null, vd.required]
-		});
-	}
-	isValid(name) {
-		return this.rForm.get(name).valid;
-	}
-	saveGameResource() {
-		this.store.dispatch(new SaveResourceAction(this.rForm.value));
-	}
-	handleFileUpload(file): void {
-		this.rForm.patchValue({ image: file });
-	}
+    public rForm: FormGroup;
+    constructor(private fb: FormBuilder, private store: Store<AppState>) {
+        this.rForm = fb.group({
+            'name': [null, vd.compose([vd.required, vd.minLength(3)])],
+            'description': [null],
+            'plural': [null],
+            'image': [null, vd.required]
+        });
+    }
+    isValid(name) {
+        return this.rForm.get(name).valid;
+    }
+    saveGameResource() {
+        this.store.dispatch(new SaveResourceAction(this.rForm.value));
+    }
+    handleFileUpload(file): void {
+        this.rForm.patchValue({ image: file });
+    }
 }
