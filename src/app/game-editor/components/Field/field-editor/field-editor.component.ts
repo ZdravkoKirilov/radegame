a0d0b9d13658c9ce62270b769dfbf59fe9870b5a
Zdravko Kirilov <zkirilov@kpmg.com>
2018-02-07 16:55:00 +0200

    field editor inherits abstract class

--- src/app/game-editor/components/Field/field-editor/field-editor.component.ts
+++ src/app/game-editor/components/Field/field-editor/field-editor.component.ts
@@ -1,41 +1,17 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
-import { BaseControl } from '../../../../dynamic-forms/models/Base.model';
 import { BoardField } from '../../../../game-mechanics/models/index';
 import { ControlsService } from '../../../../dynamic-forms/services/controls.service';
-import { FormDefinition } from '../../../../dynamic-forms/models/FormDefinition.model';
-import { ConnectedEntities } from '../../../../dynamic-forms/models/ConnectedEntities';
+import { EditorBase } from '../../mixins/editor.base';
 
 @Component({
     selector: 'rg-field-editor',
     templateUrl: './field-editor.component.html',
     styleUrls: ['./field-editor.component.scss']
 })
-export class FieldEditorComponent implements OnInit {
-    @Output() save: EventEmitter<BoardField> = new EventEmitter();
-    @Output() cancel: EventEmitter<any> = new EventEmitter();
+export class FieldEditorComponent extends EditorBase<BoardField> {
 
-    @Input() formDefinition: FormDefinition;
-    @Input() selectedItem: BoardField;
-    @Input() connectedEntities: ConnectedEntities;
-
-    public form: FormGroup;
-    public controls: BaseControl[];
-
-    constructor(private cs: ControlsService) {
-    }
-
-    saveItem() {
-        this.save.emit(this.form.value);
-    }
-
-    cancelAction() {
-        this.cancel.emit();
-    }
-
-    ngOnInit() {
-        this.controls = this.formDefinition(this.selectedItem, this.connectedEntities);
-        this.form = this.cs.toFormGroup(this.controls);
+    constructor(public cs: ControlsService) {
+        super(cs);
     }
 }
