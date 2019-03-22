864861c3a1b187d8cb6499ea6c5d5ff15c3d720c
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2019-03-22 10:53:01 +0200

    @rendering/animations: further progress

--- src/app/rendering/animations/base.ts
+++ src/app/rendering/animations/base.ts
@@ -1,41 +1,71 @@
 import { Tween } from "@tweenjs/tween.js";
-import { Subject } from "rxjs";
 
-import { Component, Styles, DidUpdatePayload, Lifecycles } from "../models";
-import { StatefulComponent } from "../mixins";
+import { Component, Styles, DidUpdatePayload } from "../models";
+import { parseValue } from "./helpers";
+
+export class AnimationOrchestrator {
+    type: 'parallel' | 'exclusive';
+    groups: AnimationGroup[];
+
+    checkAll(data: DidUpdatePayload) {
+        const eligibleGroups = this.groups.filter(group => group.isEligible(data));
+        return eligibleGroups;
+    }
+
+    playAll(groups: AnimationGroup[]) {
+        return Promise.all([...groups.map(group => group.playAll())])
+    }
+
+    playAllEligible(data: DidUpdatePayload) {
+        const eligible = this.checkAll(data);
+
+        if (this.type === 'exclusive') {
+            this.stopOthers(eligible);
+        }
+        return this.playAll(eligible);
+    }
+
+    stopOthers(exceptions: AnimationGroup[]) {
+        return this.groups.forEach(elem => {
+            if (exceptions.indexOf(elem) === -1) {
+                elem.stopAll();
+            }
+        });
+    }
+}
+
 
 export class AnimationGroup {
-    type: 'parallel' | 'sequence' | 'exclusive';
+    type: 'parallel' | 'sequence';
     transition: string;   // done => undone, hot <=> cold etc.
     prop: string;   //  state.pesho, props.gosho
     animations: AnimationBase[];
-    gap?: number; // in case of type=sequence
 
     async playAll() {
         if (this.type === 'parallel') {
             return Promise.all(this.animations.map(animation => animation.playAll()));
+        } else { // sequence
+            return this.animations.reduce((total, animation) => {
+                return animation.playAll();
+            }, Promise.all([]));
         }
     }
 
     async playIfEligible(data: DidUpdatePayload) {
-        if ('transition is correct') {
+        if (this.isEligible(data)) {
             return this.playAll();
         } else {
             return Promise.resolve();
         }
     }
-}
 
-// Note to myself: pass the animation object directly to children
-// then children .push directly themselves into the components[] of the animation
-// then the parent component -> onUpdate - animations.forEach -> playAll()
+    isEligible(data: DidUpdatePayload) {
+        return true;
+    }
 
-export type AnimationConfig<T> = {
-    id: string | number,
-    easing: (data: number) => number,
-    timing: number,
-    expected?: T,
-    initial?: T,
+    stopAll() {
+        return this.animations.forEach(animation => animation.stop());
+    }
 }
 
 export class AnimationBase<T = Partial<Styles>> {
@@ -45,6 +75,8 @@ export class AnimationBase<T = Partial<Styles>> {
         tween: Tween;
         component: Component;
     }> = [];
+
+    constructor(private config: AnimationConfig) { }
 
     addComponent(component: Component) {
         this.components.add(component);
@@ -58,32 +90,28 @@ export class AnimationBase<T = Partial<Styles>> {
         return this.active.some(elem => elem.component === component);
     }
 
-    complete$ = new Subject<Component>();
-    start$ = new Subject<Component>();
-    pause$ = new Subject<Component>();
-
-    constructor(
-        public id: string | number,
-        public easing: (data: number) => number,
-        public timing: number,
-        public expected?: T,
-        public initial?: T,
-    ) { }
-
     playAll() {
         return Promise.all(Array.from(this.components).map(comp => this.play(comp)));
     }
 
     play(target: Component) {
+        let { expected, initial, timing, easing, repeat = 0, delay = 0, yoyo = false } = this.config;
+
+        expected = this.parseValues(expected, target);
+        initial = this.parseValues(initial, target);
+
         return new Promise((resolve) => {
-            const tween = new Tween(this.initial)
-                .to(this.expected, this.timing)
-                .easing(this.easing)
+            const tween = new Tween(initial)
+                .to(expected, timing)
+                .easing(easing)
+                .delay(delay)
                 .onUpdate((data: T) => {
                     this.update(target, data);
                 })
+                .onStop(() => {
+
+                })
                 .onComplete(() => {
-                    this.complete$.next(target);
                     resolve(target);
                 });
 
@@ -91,11 +119,23 @@ export class AnimationBase<T = Partial<Styles>> {
                 tween, component: target
             });
 
-            // tween.repeat(Infinity);
-            // tween.yoyo(true);
+            tween.repeat(repeat);
+            tween.yoyo(yoyo);
 
             tween.start();
         });
+    }
+
+    parseValues(from: Partial<Styles>, comp: Component) {
+        const transformed = Object.keys(from).reduce((acc, key) => {
+            const value = parseValue(from[key]);
+
+            acc[key] = value;
+            return acc;
+        }, {});
+
+
+        return transformed;
     }
 
     update(target: Component, data: T) {
@@ -111,7 +151,8 @@ export class AnimationBase<T = Partial<Styles>> {
     }
 
     stop() {
-
+        this.active.forEach(({ tween }) => tween.stop());
+        this.active = [];
     }
 }
 
@@ -130,4 +171,15 @@ export function WithAnimations(animations: AnimationGroup[] = []) {
         };
         constructor.prototype.animations = animations;
     }
+}
+
+export type AnimationConfig<T = any> = {
+    id: string | number,
+    easing: (data: number) => number,
+    timing: number,
+    expected?: T,
+    initial?: T,
+    repeat?: number;
+    yoyo?: boolean;
+    delay?: number;
 }
---
+++ src/app/rendering/animations/helpers.ts
@@ -0,0 +1,9 @@
+const SPECIALS = ['+', '-', '*']
+
+const isSpecialValue = () => {
+
+};
+
+export const parseValue = (value: string | number) => {
+    return value;
+};
