03457a368024b3dea1926e707a1eb569fac4d321
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2019-03-26 16:29:02 +0200

    latest progress

--- src/app/rendering/components/Scrollable/index.ts
+++ src/app/rendering/components/Scrollable/index.ts
@@ -11,6 +11,7 @@ export type ScrollableProps = Partial<{
     borderColor?: number;
     borderSize?: number;
     padding?: string;
+    background?: number;
 }>
 
 type State = {
@@ -22,12 +23,15 @@ export class Scrollable extends StatefulComponent<ScrollableProps, State> {
     state: State = { xScroll: 0, yScroll: 0 };
 
     render() {
-        const { width, height, x, y, children, borderColor, borderSize, padding } = this.props;
+        const { width, height, x, y, children, borderColor, borderSize = 0,
+            padding = '0 0', background = 0xffffff } = this.props;
         const { xScroll, yScroll } = this.state;
-        const [paddingX, paddingY] = (padding || '0 0')
+        const [paddingX, paddingY] = padding
             .split(' ')
-            .map(elem => Number(elem) + (borderSize || 0))
+            .map(elem => Number(elem) + borderSize)
             .filter(elem => !isNaN(elem));
+
+
         return createElement(
             'container',
             { styles: { x, y, mask: [x, y, width, height] } },
@@ -35,7 +39,7 @@ export class Scrollable extends StatefulComponent<ScrollableProps, State> {
                 styles: {
                     width, height, x: 0, y: 0,
                     strokeColor: borderColor, strokeThickness: borderSize,
-                    fill: 0xff9999
+                    fill: background
                 }
             }),
             createElement(
@@ -45,8 +49,8 @@ export class Scrollable extends StatefulComponent<ScrollableProps, State> {
                     scrollable: {
                         yThreshold: 50,
                         xThreshold: 50,
-                        maxY: '0',
-                        minY: `(height - ${height} + (${paddingX} * 2)) * -1`
+                        maxY: `0 - ${paddingY}`,
+                        minY: `(height - ${height} + (${paddingY} * 2)) * -1`
                     },
                     onScroll: this.onChildScroll
                 },
