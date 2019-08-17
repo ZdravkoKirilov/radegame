791992159da5c5784c947e96270a336e70c95a58
ZdravkoKirilov <zdravkokirilov@yahoo.com>
2019-08-17 22:06:43 +0300

    Choice updates

--- src/app/game-editor/forms/choice/index.ts
+++ src/app/game-editor/forms/choice/index.ts
@@ -1,21 +1,34 @@
 import { Choice } from '@app/game-mechanics';
 import { BaseControl, ConnectedEntities, parse } from '@app/dynamic-forms';
 import {
-    baseTemplate, composeCommonFormContext
+    baseTemplate, composeCommonFormContext, keywordsTemplate
 } from '../helpers';
 
 export function composeChoiceForm(data: Choice, ent: ConnectedEntities): BaseControl[] {
     data = data || {};
     const options = data.options || [];
+    const tips = data.tips || [];
+    const keywords = data.keywords || [];
 
     const template = `
     <Form>
         ${baseTemplate}
 
-        <ButtonGroup name='random' label='Random' options='{boolean_options}'>{data.random}</ButtonGroup>
+        <Dropdown name='chances' label='Chances' options='{expression_options}'>
+            {data.chances}
+        </Dropdown>
+
+        <Dropdown name='time' label='Time limit' options='{expression_options}'>
+            {data.time}
+        </Dropdown>
+
+        <Dropdown name='options_filter' label='Options filter' options='{expression_options}'>
+            {data.options_filter}
+        </Dropdown>
+
+        ${keywordsTemplate}
 
         <Group name='options' label='Options' children='{options}' item='@item' addButtonText='Add'>
-
             <Form>
                 <NumberInput name='id' hidden='{true}'>{@item.id}</NumberInput>
 
@@ -26,9 +39,27 @@ export function composeChoiceForm(data: Choice, ent: ConnectedEntities): BaseCon
                 <Dropdown name='image' label='Image' options='{image_options}' showImage='{true}'>{data.image}</Dropdown>
 
                 <Dropdown name='effect' label='Effect' options='{expression_options}'>{@item.effect}</Dropdown>
+
+                <ButtonGroup name='keywords' label='Keywords' options='{keyword_options}' multiple='{true}'>
+                    {@item.keywords}
+                </ButtonGroup>
                 
             </Form>
+        </Group>
 
+        <Group name='tips' label='Tips' children='{tips}' item='@tip' addButtonText='Add'>
+            <Form>
+                <NumberInput name='id' hidden='{true}'>{@tip.id}</NumberInput>
+
+                <TextInput name='description' label='Description'>{@tip.description}</TextInput>
+
+                <Dropdown name='image' label='Image' options='{image_options}'>{@tip.image}</Dropdown>
+
+                <ButtonGroup name='keywords' label='Keywords' options='{keyword_options}' multiple='{true}'>
+                    {@tip.keywords}
+                </ButtonGroup>
+                
+            </Form>
         </Group>
 
     </Form>
@@ -38,7 +69,7 @@ export function composeChoiceForm(data: Choice, ent: ConnectedEntities): BaseCon
         source: template,
         context: {
             ...composeCommonFormContext(data, ent),
-            data, options,
+            data, options, keywords, tips,
         },
     }, true);
 
--- src/app/game-mechanics/entities/Choice.model.ts
+++ src/app/game-mechanics/entities/Choice.model.ts
@@ -1,12 +1,23 @@
-import { BaseModel, } from "./Base.model";
+import { BaseModel, WithKeywords, } from "./Base.model";
 import { Omit } from "@app/shared";
 
-export type Choice = BaseModel & Partial<{
-    random: boolean;  // could have a random choice option
+export type Choice = BaseModel & WithKeywords & Partial<{
+    chances: number; // retries. Expression
+    time: number; // Expression
+
+    options_filter: number; // Expression
+
     options: number[]; //ChoiceOption[];
+    tips: number[]; // ChoiceTip[];
 }>
 
-export type ChoiceOption = Omit<BaseModel, 'game'> & Partial<{
+export type ChoiceOption = Omit<BaseModel, 'game'> & WithKeywords & Partial<{
     owner: number; // Choice
     effect: number; // Expression
+}>
+
+export type ChoiceTip = WithKeywords & Partial<{
+    owner: number; // Choice
+    description: string;
+    image: number; // ImageAsset
 }>
